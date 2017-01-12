4b2851e466d88e0332bf940d060e4b327b63b717
Ingo Haschler <ih@ingohaschler.de>
2017-01-12 20:59:29 +0100

    Musterlösung für Zeitmessung Wurzel.

---
+++ Wurzel/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+
+
+// Zieht die Quadratwurzel aus einer reellen Zahl.
+// Parameter:
+//  zahl: Die Zahl, aus der die Wurzel gezogen wird
+//  fehler: Der maximale Fehler für die Berechnung
+// Rückgabe: Das Ergebnis der Berechnung
+double wurzel(double zahl, double fehler)
+{
+    double a = 1;
+    double b = zahl;
+    // Schleife ergibt sich aus Muster im PAP
+    do
+    {
+        b = (a + b) / 2;
+        a = zahl / b;
+    }
+    while(!(b - a < fehler));
+
+    return b;
+}
+
+// Ruft die Funktion wurzel mit einer vorgegebenen Genauigkeit auf
+// Parameter, Rückgabe: s. dort...
+double wurzel(double zahl)
+{
+    return wurzel(zahl, 0.000001);
+}
+
+int main()
+{
+    // Messschleife
+    for(int i = 0; i < 100000000; i += 10)
+    {
+        clock_t start = clock();
+        // Mehrfach ausführen, um die Auflösung zu erhöhen
+        for(int j = 0; j < 1000; j += 1)
+        {
+
+            double x = i / 100000000;
+                wurzel(x);
+        }
+        clock_t end = clock();
+
+        double runtime = (double)(end - start) ;
+
+        cout << i << ", " << runtime << endl;
+    }
+
+}
