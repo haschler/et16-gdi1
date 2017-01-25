28d8008173089b27eb50ddd10609869c41994fbd
Ingo Haschler <lehre@itih.de>
2017-01-25 13:30:26 +0000

    Besser: Mit Funktion (Modularisierung).

--- wurzel-nach-plan.cc
+++ wurzel-nach-plan.cc
@@ -2,14 +2,13 @@
 
 using namespace std;
 
-int main()
-{   // entspricht Start im PAP
-    double zahl;
-    cout << "Zahl: ";
-    cin >> zahl;
-    double fehler;
-    cout << "Fehler: ";
-    cin >> fehler;
+// Zieht die Quadratwurzel aus einer reellen Zahl.
+// Parameter:
+//  zahl: Die Zahl, aus der die Wurzel gezogen wird
+//  fehler: Der maximale Fehler für die Berechnung
+// Rückgabe: Das Ergebnis der Berechnung
+double wurzel(double zahl, double fehler)
+{
     double a = 1;
     double b = zahl;
     // Schleife ergibt sich aus Muster im PAP
@@ -19,7 +18,19 @@ int main()
         a = zahl / b;
     }
     while(!(b - a < fehler));
+    
+    return b;
+}
 
-    cout << b << endl;
+int main()
+{   // entspricht Start im PAP
+    double zahl;
+    cout << "Zahl: ";
+    cin >> zahl;
+    double fehler;
+    cout << "Fehler: ";
+    cin >> fehler;
+    
+    cout << wurzel(zahl, fehler) << endl;
     return 0; // entspricht Stop im PAP
 }
