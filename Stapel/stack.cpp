32f4c42df17e719f67538d032bce151c3adda4b8
Ingo Haschler <lehre@itih.de>
2017-02-01 13:41:47 +0000

    Push fertig.

--- Stapel/stack.cpp
+++ Stapel/stack.cpp
@@ -29,7 +29,8 @@ struct STAPEL
 {
     int* feld;
     int n;
-} typedef struct STAPEL Stapel;
+};
+typedef struct STAPEL Stapel;
 
 void neuerStapel(Stapel*& neu)
 {
@@ -42,12 +43,34 @@ bool stapelLeer(Stapel* stapel)
     return(stapel->n == 0);
 }
 
+void stapelPush(Stapel* stapel, int zahl)
+{
+    // neues Feld (n+1) anlegen
+    int* neu = new int[stapel->n + 1];
+    // alle Elemente kopieren
+    for(int i = 0; i < stapel->n; i += 1)
+        neu[i] = stapel->feld[i];
+    // neues Element anfügen
+    neu[stapel->n] = zahl;
+    // Feldgröße aktualisieren
+    stapel->n += 1;
+    // altes Feld löschen
+    delete(stapel->feld);
+    // Zeiger anpassen
+    stapel->feld = neu;
+}
+
 
 // Hauptprogramm
 int main()
 {
     Stapel* s;
     neuerStapel(s);    
+    if(stapelLeer(s))
+        cout << "Stapel ist leer" << endl;
+    stapelPush(s, 1);
+    stapelPush(s, 2);
+    stapelPush(s, 3);
     if(stapelLeer(s))
         cout << "Stapel ist leer" << endl;
     
